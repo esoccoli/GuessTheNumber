4c64567e06657893be4e732e269735a6f8bb0b30
Ella S <efs8086@rit.edu>
2022-09-15 20:55:04 -0400

    Prompts user to guess a number and checks if their guess is correct

--- GuessTheNumber.py
+++ GuessTheNumber.py
@@ -1,6 +1,20 @@
 import random
 
+print("Welcome to the Guess the Number game!")
+print("")
+
 number = random.randint(1,100)
 
+guess = 0
 
+while(guess != number):
+    guess = int(input("Guess a number between 1 and 100. "))
 
+    if (guess < number):
+        print("Too small. Guess a larger number.")
+
+    elif (guess > number):
+        print("Too large. Guess a smaller number.")
+
+    elif (guess == number):
+        print("Correct! You win!")
